0d2badd16ee275e6b55a38a035027a48dbe02634
Marcus Priesch <marcus@priesch.co.at>
2004-12-01 16:15:43 +0000

    Recent changes

--- time/detectors/user.py
+++ time/detectors/user.py
@@ -154,7 +154,7 @@ def init(db):
     # fire before changes are made
     db.user.audit('set'   , audit_user_fields)
     db.user.audit('create', audit_user_fields)
-    db.user.react("create", update_userlist_html)
+#    db.user.react("create", update_userlist_html)
     db.user.react("set"   , update_userlist_html)
 
 # vim: set filetype=python ts=4 sw=4 et si
