8b78335ce4c2ced2236e2e880b9a7467a67dbe73
Ralf Schlatterbeck <rsc@runtux.com>
2005-04-13 09:28:52 +0000

    Factored from dbinit

---
+++ abo/initial_data.py
@@ -0,0 +1,135 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2004 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+# $Id$
+
+#
+# INITIAL VALUES
+#
+
+# create the two default users
+user = db.getclass('user')
+user.create \
+    ( username="admin"
+    , password=adminpw
+    , address=config.ADMIN_EMAIL
+    , roles='Admin'
+    )
+user.create (username="anonymous", roles='Anonymous')
+
+currency = db.getclass ('currency')
+currency.create (name = 'CHF', description = 'Schweizer Franken')
+currency.create (name = 'EUR', description = 'Euro')
+currency.create (name = 'GBP', description = 'Britische Pfund')
+currency.create (name = 'USD', description = 'US-Dollar')
+
+abo_type = db.getclass ('abo_type')
+abo_type.create \
+    ( name        = 'ZFW12norm'
+    , description = uni ('Jahresabo Zeit-Fragen')
+    , order       = 1
+    )
+abo_type.create \
+    ( name        = 'ZFW06norm'
+    , description = uni ('Halbjahresabo Zeit-Fragen')
+    , order       = 2
+    )
+abo_type.create \
+    ( name        = 'ZFW12gift'
+    , description = uni ('Geschenkabo Zeit-Fragen')
+    , order       = 3
+    )
+abo_type.create \
+    ( name        = 'CCM12norm'
+    , description = uni ('Subscription Current Concerns')
+    , order       = 4
+    )
+abo_type.create \
+    ( name        = 'HDM12norm'
+    , description = uni ('abonnement Horizons et débats')
+    , order       = 5
+    )
+abo_type.create \
+    ( name        = 'ZFM12norm'
+    , description = uni ('Jahresabo Zeit-Fragen Monatsausgabe')
+    , order       = 6
+    )
+abo_type.create \
+    ( name        = 'ZFM12gift'
+    , description = uni ('Geschenkabo Zeit-Fragen Monatsausgabe')
+    , order       = 7
+    )
+abo_type.create \
+    ( name        = 'ZFM12spon'
+    , description = uni ('Förderabo Zeit-Fragen Monatsausgabe')
+    , order       = 8
+    )
+abo_type.create \
+    ( name        = 'ZFW12free'
+    , description = uni ('Gratisabo Zeit-Fragen')
+    , order       = 9
+    )
+abo_type.create \
+    ( name        = 'ZFW12spec'
+    , description = uni ('Spezialabo Zeit-Fragen')
+    , order       = 10
+    )
+abo_type.create \
+    ( name        = 'CCM12gift'
+    , description = uni ('Gift subscription Current Concerns')
+    , order       = 11
+    )
+abo_type.create \
+    ( name        = 'CCM12free'
+    , description = uni ('Free subscription Current Concerns')
+    , order       = 12
+    )
+abo_type.create \
+    ( name        = 'HDM12gift'
+    , description = uni ('abonnement-cadeau Horizons et débats')
+    , order       = 13
+    )
+abo_type.create \
+    ( name        = 'ZFW12pate'
+    , description = uni ('Patenschaftsabo Zeit-Fragen')
+    , order       = 14
+    )
+abo_type.create \
+    ( name        = 'ZFW06gift'
+    , description = uni ('Halbjahres-Geschenkabo Zeit-Fragen')
+    , order       = 15
+    )
+abo_type.create \
+    ( name        = 'ZFW06spec'
+    , description = uni ('Halbjahres-Spezialabo Zeit-Fragen')
+    , order       = 16
+    )
+
+valid = db.getclass ('valid')
+valid.create \
+    ( name = uni ('gültig')
+    , description = uni ('Gültige Adresse')
+    )
+valid.create \
+    ( name = uni ('verstorben')
+    , description = uni ('Verstorbener Adressat')
+    )
---
+++ abo/schema.py
@@ -0,0 +1,245 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2004 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+# $Id$
+
+# Class automatically gets these properties:
+#   creation = Date ()
+#   activity = Date ()
+#   creator  = Link ('user')
+
+do_index = "yes"
+
+abo = Class \
+    ( db, "abo"
+    , begin               = Date      ()
+    , end                 = Date      ()
+    , aboprice            = Link      ('abo_price')
+    , payer               = Link      ('address')
+    , subscriber          = Link      ('address')
+    , amount              = Number    ()
+    , messages            = Multilink ("msg")
+    )
+
+abo_price = Class \
+    ( db, "abo_price"
+    , abotype             = Link      ('abo_type')
+    , currency            = Link      ('currency')
+    , amount              = Number    ()
+    , name                = String    ()
+    , invoice_template    = Multilink ('invoice_template')
+    )
+abo_price.setkey ("name")
+
+abo_type = Class \
+    ( db, "abo_type"
+    , name                = String    ()
+    , description         = String    ()
+    , order               = Number    ()
+    )
+abo_type.setkey ("name")
+
+address = Class \
+    ( db, "address"
+    , title               = String    ()
+    , lettertitle         = String    ()
+    , firstname           = String    (indexme = do_index)
+    , initial             = String    ()
+    , lastname            = String    (indexme = do_index)
+    , function            = String    (indexme = do_index)
+    , street              = String    (indexme = do_index)
+    , country             = String    ()
+    , postalcode          = String    ()
+    , city                = String    (indexme = do_index)
+    , phone               = String    ()
+    , fax                 = String    ()
+    , salutation          = String    ()
+    , messages            = Multilink ("msg")
+    , email               = String    (indexme = do_index)
+    , abos                = Multilink ("abo")
+    , payed_abos          = Multilink ("abo")
+    , adr_type            = Multilink ("adr_type")
+    , valid               = Link      ("valid")
+    )
+
+adr_type = Class \
+    ( db, "adr_type"
+    , code                = String    ()
+    , description         = String    ()
+    )
+adr_type.setkey ("code")
+
+currency = Class \
+    ( db, "currency"
+    , name                = String    ()
+    , description         = String    ()
+    )
+currency.setkey ("name")
+
+invoice = Class \
+    ( db, "invoice"
+    , invoice_no          = String    ()
+    , period_start        = Date      ()
+    , period_end          = Date      ()
+    , amount              = Number    ()
+    , currency            = Link      ("currency")
+    , balance_open        = Number    ()
+    , n_sent              = Number    ()
+    , last_sent           = Date      ()
+    , abo                 = Link      ("abo")
+    , date_payed          = Date      ()
+    , bookentry           = Date      ()
+    , receipt_no          = String    ()
+    , send_it             = Boolean   ()
+    )
+invoice.setkey ("invoice_no")
+
+# The invoice_level number decides for which invoice level we use
+# which template.
+invoice_template = Class \
+    ( db, "invoice_template"
+    , template            = Link      ("template")
+    , invoice_level       = Number    ()
+    )
+
+# head should contain info for recreating a letter with multiple
+# destinations. In this case the file exists only once and the
+# customisation info in head is there for each recipient.
+# The file types are either PDF (from old imported data) or an
+# OpenOffice.org document which is cusomized using info in head.
+letter = Class \
+    ( db, "letter"
+    , address             = Link      ("address")
+    , date                = Date      ()
+    , letter              = Link      ("file")
+    , note                = Link      ("msg")
+    , head                = String    ()
+    )
+
+template = Class \
+    ( db, "template"
+    , name                = String    ()
+    , template            = Multilink ("file") # version control, use latest
+    )
+
+# Define codes for (in)valid addresses, e.g., "verstorben"
+valid = Class \
+    ( db, "valid"
+    , name                = String    ()
+    , description         = String    ()
+    )
+valid.setkey ("name")
+
+query = Class \
+    ( db, "query"
+    , klass               = String    ()
+    , name                = String    ()
+    , url                 = String    ()
+    , private_for         = Link      ('user')
+    )
+query.setkey("name")
+
+
+# Note: roles is a comma-separated string of Role names
+user = Class \
+    ( db, "user"
+    , username            = String    ()
+    , password            = Password  ()
+    # used by email gateway:
+    , address             = String    ()
+    , realname            = String    ()
+    , alternate_addresses = String    ()
+    # other framework parts in rup
+    , queries             = Multilink ('query')
+    , roles               = String    ()
+    , timezone            = String    ()
+    # optional
+    , nickname            = String    ()
+    )
+user.setkey("username")
+
+# FileClass automatically gets these properties:
+#   content = String()    [saved to disk in <tracker home>/db/files/]
+#   (it also gets the Class properties creation, activity and creator)
+msg = FileClass \
+    ( db, "msg"
+    , date                  = Date      ()
+    # Note: below fields are used by roundup internally (obviously by the
+    #       mail-gateway)
+    , author                = Link      ("user", do_journal='no')
+    , recipients            = Multilink ("user", do_journal='no')
+    , summary               = String    ()
+    , messageid             = String    ()
+    , inreplyto             = String    ()
+    )
+
+file = FileClass \
+    ( db, "file"
+    , name                  = String    ()
+    , type                  = String    ()
+    )
+
+#
+# SECURITY SETTINGS
+#
+classes = \
+    [ ("abo_price"         , ["User"], ["Product"       ])
+    , ("abo_type"          , ["User"], ["Product"       ])
+    , ("abo"               , ["User"], ["Abo"           ])
+    , ("address"           , ["User"], ["User"          ])
+    , ("adr_type"          , ["User"], ["Type"          ])
+    , ("currency"          , ["User"], ["Product"       ])
+    , ("file"              , ["User"], ["User"          ])
+    , ("invoice_template"  , ["User"], ["Invoice"       ])
+    , ("invoice"           , ["User"], ["Invoice"       ])
+    , ("letter"            , ["User"], ["Abo"           ])
+    , ("msg"               , ["User"], ["User"          ])
+    , ("query"             , ["User"], ["User"          ])
+    , ("template"          , ["User"], ["Abo", "Invoice"])
+    , ("user"              , ["User"], ["Admin"         ])
+    , ("valid"             , ["User"], ["Admin"         ])
+    ]
+
+roles = \
+    [ ("Abo"           , "Allowed to modify subscriptions")
+    , ("Invoice"       , "Allowed to change financial data")
+    , ("Product"       , "Allowed to create/edit products")
+    , ("Type"          , "Allowed to add/change type codes")
+    ]
+for name, desc in roles :
+    db.security.addRole (name = name, description = desc)
+
+
+# Assign the access and edit permissions
+for cl, view_list, edit_list in classes : \
+    for viewer in view_list :
+        db.security.addPermissionToRole (viewer, 'View', cl)
+    for editor in edit_list :
+        db.security.addPermissionToRole (editor, 'Edit',   cl)
+        db.security.addPermissionToRole (editor, 'Create', cl)
+
+# and give the regular users access to the web and email interface
+db.security.addPermissionToRole('User', 'Web Access')
+db.security.addPermissionToRole('User', 'Email Access')
+
+# Anonymous may view other users profiles - required for intranet pages.
+db.security.addPermissionToRole('Anonymous', 'View', 'user')
