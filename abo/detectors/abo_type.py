6db754149fbbd7f54356caf52242ac4ac003a014
Ralf Schlatterbeck <rsc@runtux.com>
2005-06-27 14:06:59 +0000

    Check for selected adr_type in abo_type
    must be in category "ABO".

--- abo/detectors/abo_type.py
+++ abo/detectors/abo_type.py
@@ -31,9 +31,14 @@ def check (db, cl, nodeid, new_values) :
             attr = cl.get (nodeid, i)
         if not attr :
             raise Reject, _ ('"%(attr)s" must be filled in') % {'attr' : _ (i)}
-    period = new_values.get ('period', cl.get (nodeid, 'period'))
+    period = new_values.get   ('period',   cl.get (nodeid, 'period'))
     if int (period) != period :
         raise Reject, _ ('period must be an integer')
+    adr_type = new_values.get ('adr_type', cl.get (nodeid, 'adr_type'))
+    cat = db.adr_type.get  (adr_type, 'typecat')
+    if db.adr_type_cat.get (cat, 'code') != 'ABO' :
+        raise Reject, _ \
+            ('Selected address types must be in type category "ABO"')
 # end def check
 
 def init (db) :
