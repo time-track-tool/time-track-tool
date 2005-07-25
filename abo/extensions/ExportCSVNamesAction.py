778597fce736a185f6479e35c8a232fd16c29fb1
Ralf Schlatterbeck <rsc@runtux.com>
2005-07-25 09:36:50 +0000

    Changes from installed tracker (latin1)

--- abo/extensions/ExportCSVNamesAction.py
+++ abo/extensions/ExportCSVNamesAction.py
@@ -1,3 +1,34 @@
+#! /usr/bin/python
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2004 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Reichergasse 131, A-3411 Weidling.
+# Web: http://www.runtux.com Email: office@runtux.com
+# All rights reserved
+# ****************************************************************************
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    ExportCSVNamesAction
+#
+# Purpose
+#    Action for exporting current query as CSV (comma separated values)
+#--
+
 from roundup.cgi.actions import Action
 from roundup.cgi import templating
 from roundup import hyperdb
@@ -6,11 +37,17 @@ import csv
 import re
 
 def repr_date (x) :
-    if x == None :
+    if x is None :
         return ""
     else :
         return x.pretty ('%Y-%m-%d')
 # end def repr_date
+
+def repr_str (x) :
+    if x is None :
+        return ""
+    return str (x).decode ('utf-8').encode ('latin1')
+# end def repr_str
 
 class Export_CSV_Names (Action) :
     name = 'export'
@@ -59,17 +96,18 @@ class Export_CSV_Names (Action) :
                 if x == None :
                     return ""
                 else :
-                    return " ".join ([str (cls.get (x, col)) for col in cols])
+                    s = " ".join ([str (cls.get (x, col)) for col in cols])
+                    return s.decode ('utf-8').encode ('latin1')
             return f
         # end def repr_link
 
         for col in columns :
-            represent [col] = str
+            represent [col] = repr_str
             if isinstance (props [col], hyperdb.Link) :
                 cn = props [col].classname
                 cl = self.db.getclass (cn)
                 pr = cl.getprops ()
-                if 'lastname' in pr :
+                if 'lastname' in pr and cl.labelprop () != 'username' :
                     represent [col] = repr_link (cl, ('firstname', 'lastname'))
                 else :
                     represent [col] = repr_link (cl, (cl.labelprop (),))
