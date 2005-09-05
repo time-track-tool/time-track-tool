b7c1c8de16b696f9752b4c5df2509e369a29ba6c
Ralf Schlatterbeck <rsc@runtux.com>
2005-09-05 12:09:11 +0000

    Duplicate check
    ...for wp "name" and "wp_no" for same project.

--- time/detectors/time_wp.py
+++ time/detectors/time_wp.py
@@ -35,11 +35,22 @@ from roundup.cgi.TranslationService import get_translation
 _      = lambda x : x
 common = None
 
+def check_duplicate_field_value (cl, project, field, value) :
+    ids     = cl.filter (None, {field : value, 'project' : project})
+    if ids :
+        assert (len (ids) == 1)
+        raise Reject, _ ('Duplicate %(field)s "%(value)s"') % locals ()
+# end def check_duplicate_field_value
+
 def check_time_wp (db, cl, nodeid, new_values) :
     for i in 'wp_no', 'project' :
         if i in new_values and cl.get (nodeid, i) :
             raise Reject, "%(attr)s may not be changed" % {'attr' : _ (i)}
     common.check_name_len (_, new_values.get ('name', cl.get (nodeid, 'name')))
+    project = cl.get (nodeid, 'project')
+    for i in 'name', 'wp_no' :
+        if i in new_values :
+            check_duplicate_field_value (cl, project, i, new_values [i])
 # end def check_time_wp
 
 def new_time_wp (db, cl, nodeid, new_values) :
@@ -49,6 +60,10 @@ def new_time_wp (db, cl, nodeid, new_values) :
     if 'durations_allowed' not in new_values :
         new_values ['durations_allowed'] = False
     common.check_name_len (_, new_values ['name'])
+    project = new_values  ['project']
+    for i in 'name', 'wp_no' :
+        if i in new_values :
+            check_duplicate_field_value (cl, project, i, new_values [i])
 # end def new_time_wp
 
 def init (db) :
