ef446fae2dbd467d6a2ceae4b90e0ed0d324321b
Ralf Schlatterbeck <rsc@runtux.com>
2005-09-30 15:50:52 +0000

    First sketch of reminder script

---
+++ time/utils/remind.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+
+import sys, os
+from roundup           import date
+from roundup           import instance
+tracker = sys.argv [1]
+sys.path.insert (1, os.path.join (tracker, 'lib'))
+from common            import clearance_by
+
+tracker = instance.open (tracker)
+db      = tracker.open  ('admin')
+
+submitted = db.daily_record_status.lookup ('submitted')
+drecs     = db.daily_record.find (status = submitted)
+users     = {}
+clearers  = {}
+for dr in drecs :
+    users [db.daily_record.get (dr, 'user')] = 1
+for u in users.iterkeys () :
+    for c in clearance_by (db, u, only_subs = True) :
+        clearers [c] = 1
+print clearers
