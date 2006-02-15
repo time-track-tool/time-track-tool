d0ece19bb8a346bc792ab40266227c3897e60d26
Ralf Schlatterbeck <rsc@runtux.com>
2006-02-15 14:38:57 +0000

    Fix templating error for new wp
    where project contains '&' (missing quoting)

--- time/extensions/extproperty.py
+++ time/extensions/extproperty.py
@@ -449,6 +449,7 @@ def new_property (context, db, classname, id, propname) :
 # end def new_property
 
 def init (instance) :
+    from urllib import quote
     global _
     _ = get_translation \
         (instance.config.TRACKER_LANGUAGE, instance.tracker_home).gettext
@@ -458,4 +459,5 @@ def init (instance) :
     instance.registerUtil ('menu_or_field',     menu_or_field)
     instance.registerUtil ('new_property',      new_property)
     instance.registerUtil ('comment_edit',      comment_edit)
+    instance.registerUtil ('quote',             quote)
 # end def init
