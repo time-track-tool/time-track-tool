52ae94fbd2e8fa4fbab80d381a61ea6a060963a5
Ralf Schlatterbeck <rsc@runtux.com>
2006-05-15 07:57:15 +0000

    Corrected detection of duplicates

--- time/duplicate_dr.py
+++ time/duplicate_dr.py
@@ -4,13 +4,13 @@ tracker = instance.open ('/roundup/tracker/ttt')
 db      = tracker.open ('admin')
 
 user    = db.user.lookup ('senn')
-for j in range (1,10) :
-    i = '2005-11-%s' % j
+for j in range (1,7) :
+    i = '2006-05-%s' % j
     dr = db.daily_record.filter \
         (None, dict (date = '%s;%s' % (i, i), user = user))
     print i, dr
     for d in dr :
-        tr = db.time_record.find (daily_record = dict ([(d, 1) for d in dr]))
+        tr = db.time_record.find (daily_record = dict (((d, 1),)))
         dv = db.daily_record.getnode (d)
         print d, tr, dv.user, dv.date, dv.status, dv.time_record
 
