1f30fd1033091fcc9df9c7d4401fb62bd2b862cd
Ralf Schlatterbeck <rsc@runtux.com>
2006-09-28 16:34:29 +0000

    Fix issue creation
    getting priority broken for 0-default...

--- time/detectors/issue.py
+++ time/detectors/issue.py
@@ -59,7 +59,9 @@ def update_eff_prio (db, cl, nodeid, new_values) :
        or 'effective_prio' in new_values
        or 'status'         in new_values
        ) :
-        prio = new_values.get ('priority', None) or cl.get (nodeid, 'priority')
+        prio = new_values.get ('priority', None)
+        if prio is None :
+            prio = cl.get (nodeid, 'priority')
         part = new_values.get ('part_of',  None)
         if not part and nodeid :
             part = cl.get (nodeid, 'part_of')
