29d8973f24859ae9b89ee09b32958072591fed32
Ralf Schlatterbeck <rsc@runtux.com>
2007-02-01 11:27:04 +0000

    Fix group import
    Now no special cases are needed for the existing data.

--- time/pam/groups.py
+++ time/pam/groups.py
@@ -32,6 +32,7 @@ def datecvt (s) :
     return Date (gmtime (int (s)))
 # end def datecvt
 
+
 prefix     = 'GROUP'
 
 KEYS = \
@@ -48,6 +49,10 @@ ld.simple_bind_s (config.BIND_DN, config.BIND_PW)
 scope = ldap.SCOPE_SUBTREE
 lu    = ld.search_s (config.BASEDN, scope, 'ou=Groups')
 tttech_wien = db.org_location.lookup ('TTTech Wien')
+tttech_paf  = db.org_location.lookup ('TTTech Germany Hettenshausen')
+group_olo = \
+    { 11260 : tttech_paf
+    }
 for item in lu :
     print "--> ", item [0]
     lusub = ld.search_s (item [0], ldap.SCOPE_ONELEVEL)
@@ -55,25 +60,26 @@ for item in lu :
         #print x [0]
         d     = x [1]
         gid   = int (d ['gidNumber'][0])
-        if gid == 10000 :
-            continue
-        if 1: #gid >= 1000 and gid < 30000 :
+        if 1 :
             print "%s: %s" % (d ['cn'][0], gid)
             try :
                 g_id = db.group.lookup (d ['cn'][0])
                 g    = db.group.getnode (g_id)
-#                db.group.set \
-#                    ( g_id
-#                    , ** dict 
-#                        ( (rk, fun (d [lk][0])) for lk, rk, fun in KEYS
-#                          if fun (d [lk][0]) != fun (g [rk])
-#                        )
-#                    )
+                db.group.set \
+                    ( g_id
+                    , ** dict 
+                        ( (rk, fun (d [lk][0])) for lk, rk, fun in KEYS
+                          if fun (d.get (lk, [None])[0]) != fun (g [rk])
+                        )
+                    )
             except KeyError :
-                pass
-#                db.group.create \
-#                    ( name         = d ['cn'][0]
-#                    , org_location = tttech_wien
-#                    , ** dict ((rk, fun (d [lk][0])) for lk, rk, fun in KEYS)
-#                    )
-#db.commit ()
+                olo = group_olo.get (gid, tttech_wien)
+                db.group.create \
+                    ( name         = d ['cn'][0]
+                    , org_location = olo
+                    , ** dict
+                        ( (rk, fun (d.get (lk, [None])[0]))
+                          for lk, rk, fun in KEYS
+                        )
+                    )
+db.commit ()
