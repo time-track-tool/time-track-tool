df23dcbcddeceedd9932fdebab99f52c21e5f4b0
Ralf Schlatterbeck <rsc@runtux.com>
2007-03-14 17:54:45 +0000

    Fix argv

--- time/utils/nosyfixer.py
+++ time/utils/nosyfixer.py
@@ -4,7 +4,7 @@ import sys
 from roundup           import date
 from roundup           import instance
 from roundup.password  import Password, encodePassword
-tracker = instance.open (argv [1])
+tracker = instance.open (sys.argv [1])
 db      = tracker.open ('admin')
 
 id = sys.argv [2]
