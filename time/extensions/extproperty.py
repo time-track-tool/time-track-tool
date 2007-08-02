2b00ab14c34382cc76ac7bf7bc3c3d37d07da0d0
Ralf Schlatterbeck <rsc@runtux.com>
2007-08-02 10:05:29 +0000

    Change for displaying empty comments
    ..by PGO with old -empty icon.

--- time/extensions/extproperty.py
+++ time/extensions/extproperty.py
@@ -9,12 +9,12 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -82,7 +82,7 @@ def comment_edit \
              """('%s%s?@template=comment"""
              """&property=%s&editable=%s"""
              """&form=%s', %d, %d)">"""
-             """<img src="@@file/comment.png" alt="C" title="%s" border="0">"""
+             """<img src="@@file/comment%s.png" alt="C" title="%s" border="0">"""
              """</a>"""
              % ( property
                , quote (str (value))
@@ -93,6 +93,7 @@ def comment_edit \
                , form
                , width
                , height
+               , ("-empty", "") [bool (value)]
                , title
                )
            )
