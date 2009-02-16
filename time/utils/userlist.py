efeca13698888eb89e2532a933ab5f57783d169d
Ralf Schlatterbeck <rsc@runtux.com>
2009-02-16 09:49:51 +0000

    Move userlist creation to rup_utils
    Call it from detector
    Also provide a utils script to create the userlist

---
+++ time/utils/userlist.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.4
+# -*- coding: iso-8859-1 -*-
+# Cron-job for updating adr-type for valid or closed abos.
+# Should be run in the early morning of every 1st of a month.
+
+import sys
+import os
+from roundup           import instance
+tracker = instance.open (os.getcwd ())
+db      = tracker.open  ('admin')
+
+from rup_utils import update_userlist_html
+
+update_userlist_html (db)
