f8bd73a617dd3ac23ecdb1293e3490ecb692e9ef
Ralf Schlatterbeck <rsc@runtux.com>
2009-02-18 00:05:26 +0000

    Use tracker from current directory

--- time/utils/nosyfixer.py
+++ time/utils/nosyfixer.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python2.4
 # -*- coding: iso-8859-1 -*-
+import os
 import sys
 from roundup           import date
 from roundup           import instance
 from roundup.password  import Password, encodePassword
-tracker = instance.open (sys.argv [1])
+tracker = instance.open (os.getcwd ())
 db      = tracker.open ('admin')
 
 # sometimes there are "None" values in the nosy history. Fix these.
 # takes the id of the issue to fix
 
-id = sys.argv [2]
+id = sys.argv [1]
 journal = db.getjournal ('issue', id)
 for line in journal :
     nodid, date, tag, action, params = line
