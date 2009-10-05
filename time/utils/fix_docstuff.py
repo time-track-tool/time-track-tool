c242120b0aaff21c369f3a553658e8d81c6d6d0d
Ralf Schlatterbeck <rsc@runtux.com>
2009-10-05 12:16:55 +0000

    Add check for empty name
    Only act on non-retired items

--- time/utils/fix_docstuff.py
+++ time/utils/fix_docstuff.py
@@ -11,16 +11,20 @@ dir     = os.getcwd ()
 tracker = instance.open (dir)
 db      = tracker.open ('admin')
 
-name_txt  = "[0-9a-zA-Z_/]+"
+name_txt  = "[0-9a-zA-Z/]+"
 name_re   = re.compile ("^%s$" % name_txt)
 
-for id in db.department.getnodeids () :
+for id in db.department.getnodeids (retired = False) :
     name = db.department.get (id, 'doc_num')
     if name and not name_re.match (name) :
         print >> sys.stderr, "Wrong name for department %s: %s" % (id, name)
 for cl in db.product_type, db.reference, db.artefact :
-    for id in cl.getnodeids () :
+    for id in cl.getnodeids (retired = False) :
         name = cl.get (id, 'name')
+        if not name :
+            print >> sys.stderr, "Wrong name (empty or None) for %s %s" \
+                % (cl.classname, id)
+            continue
         if not name_re.match (name) :
             print >> sys.stderr, \
                 "Wrong name for %s %s: %s" % (cl.classname, id, name)
@@ -30,7 +34,7 @@ for id in db.doc_status.getnodeids () :
     n = db.doc_status.getnode (id)
     stati [n.name] = id
 in_progress = stati ['work in progress']
-for id in db.doc.getnodeids () :
+for id in db.doc.getnodeids (retired = False) :
     owner = db.doc.get (id, 'owner')
     resp  = db.doc.get (id, 'responsible')
     if owner and not resp :
