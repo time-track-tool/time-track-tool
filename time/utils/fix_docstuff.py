e03096b0de7c521e3fdb7f03a821c15db7a49ed8
Ralf Schlatterbeck <rsc@runtux.com>
2009-10-06 08:45:19 +0000

    Allow '-' in reference

--- time/utils/fix_docstuff.py
+++ time/utils/fix_docstuff.py
@@ -11,21 +11,22 @@ dir     = os.getcwd ()
 tracker = instance.open (dir)
 db      = tracker.open ('admin')
 
-name_txt  = "[0-9a-zA-Z/]+"
-name_re   = re.compile ("^%s$" % name_txt)
+name_re   = re.compile ("^[0-9a-zA-Z/]+$")
+ref_re    = re.compile ("^[-0-9a-zA-Z/]+$")
 
 for id in db.department.getnodeids (retired = False) :
     name = db.department.get (id, 'doc_num')
     if name and not name_re.match (name) :
         print >> sys.stderr, "Wrong name for department %s: %s" % (id, name)
-for cl in db.product_type, db.reference, db.artefact :
+for cl, regex in \
+    (db.product_type, name_re), (db.reference, ref_re), (db.artefact, name_re) :
     for id in cl.getnodeids (retired = False) :
         name = cl.get (id, 'name')
         if not name :
             print >> sys.stderr, "Wrong name (empty or None) for %s %s" \
                 % (cl.classname, id)
             continue
-        if not name_re.match (name) :
+        if not regex.match (name) :
             print >> sys.stderr, \
                 "Wrong name for %s %s: %s" % (cl.classname, id, name)
 
