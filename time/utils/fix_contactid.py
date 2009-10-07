749561a802cb47fc44f39e8fa6b4650820ce7b23
Ralf Schlatterbeck <rsc@runtux.com>
2009-10-07 14:46:07 +0000

    Fix contact id maximum (setid)

---
+++ time/utils/fix_contactid.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+import sys
+import os
+import re
+from roundup           import date
+from roundup           import instance
+dir     = os.getcwd ()
+
+tracker = instance.open (dir)
+db      = tracker.open ('admin')
+
+last = 0
+for id in db.contact.getnodeids () :
+    last = max (last, int (id))
+db.setid ('contact', str (last + 1))
+db.commit ()
+
