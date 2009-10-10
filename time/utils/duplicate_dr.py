728356bca0e5335e1fa17e629276a2e6e7833ce0
Ralf Schlatterbeck <rsc@runtux.com>
2009-10-10 16:06:02 +0000

    Duplicate DR script
    Fix python bang-path
    New user + date :-)
    Make executable

--- time/utils/duplicate_dr.py
+++ time/utils/duplicate_dr.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python
 from roundup           import date
 from roundup           import instance
 tracker  = instance.open ('/roundup/tracker/ttt')
 db       = tracker.open ('admin')
 monthd   = [21,28,31,30,31,30,31,31,30,31,30,31]
 
-user     = db.user.lookup ('erkinger')
-daystart = 14
+user     = db.user.lookup ('schlatterbeck')
+daystart = 10
 month    = 05
 
 for j in range (7) :
     max = monthd [month -1] + 1
-    i = '2007-%s-%s' % ((j + daystart) / max + month, j + daystart)
+    i = '2009-%s-%s' % ((j + daystart) / max + month, j + daystart)
     dr = db.daily_record.filter \
         (None, dict (date = '%s;%s' % (i, i), user = user))
     for d in dr :
