a749f74a30f9e4d18cc28259d9644a2c5e4874fc
Ralf Schlatterbeck <rsc@runtux.com>
2010-02-23 09:35:35 +0000

    Default values for message author and date

--- time/detectors/message.py
+++ time/detectors/message.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2006 Dr. Ralf Schlatterbeck Open Source Consulting.
+# Copyright (C) 2006-10 Dr. Ralf Schlatterbeck Open Source Consulting.
 # Reichergasse 131, A-3411 Weidling.
 # Web: http://www.runtux.com Email: office@runtux.com
 # All rights reserved
@@ -23,17 +23,27 @@
 #
 
 from roundup.mailgw import parseContent
+from roundup.date   import Date
 
-def summarygenerator(db, cl, nodeid, newvalues):
+def summarygenerator (db, cl, nodeid, newvalues) :
     ''' If the message doesn't have a summary, make one for it.
     '''
-    if newvalues.has_key('summary') or not newvalues.has_key('content'):
+    if newvalues.has_key ('summary') or not newvalues.has_key ('content') :
         return
 
-    summary, content = parseContent(newvalues['content'], 1, 1)
-    newvalues['summary'] = summary
+    summary, content = parseContent (newvalues ['content'], 1, 1)
+    newvalues ['summary'] = summary
+# end def summarygenerator
 
+def check_params (db, cl, nodeid, newvalues) :
+    """ Check if message has an author, use creator if not """
+    if 'author' not in newvalues :
+        newvalues ['author'] = db.getuid ()
+    if 'date' not in newvalues :
+        newvalues ['date'] = Date ('.')
+# end def check_params
 
 def init(db):
     # fire before changes are made
     db.msg.audit('create', summarygenerator)
+    db.msg.audit('create', check_params)
