daa65b890b54bc3e3a602ac97a7775c6630079ca
Ralf Schlatterbeck <rsc@runtux.com>
2010-03-13 15:58:19 +0000

    Add test code

--- time/utils/proplist.py
+++ time/utils/proplist.py
@@ -10,28 +10,33 @@ as_list = False
 if len (sys.argv) > 1 and sys.argv [1] == '--as_list' :
     as_list = True
 
-#print db.classes
-
 if as_list :
-    print "    properties = \\"
+    print "properties = \\"
 for clcnt, cl in enumerate (sorted (db.getclasses ())) :
     if as_list :
         o = ','
         if clcnt == 0 :
             o = '['
-        print "        %s ( '%s'" % (o, cl)
+        print "    %s ( '%s'" % (o, cl)
     else :
         print cl
     for n, p in enumerate (sorted (db.getclass (cl).properties.keys ())) :
         if as_list :
             if n :
-                print "            , '%s'" % p
+                print "        , '%s'" % p
             else :
-                print "          , [ '%s'" % p
+                print "      , [ '%s'" % p
         else :
             print "    ", p
     if as_list :
-        print "            ]"
-        print "          )"
+        print "        ]"
+        print "      )"
 if as_list :
-    print "        ]"
+    print "    ]"
+    print
+    print "if __name__ == '__main__' :"
+    print "    for cl, props in properties :"
+    print "        print cl"
+    print "        for p in props :"
+    print "            print '    ', p"
+
