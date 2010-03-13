a7ee3499168a563a51bf84b9dae76e89462d33d0
Ralf Schlatterbeck <rsc@runtux.com>
2010-03-13 17:26:39 +0000

    Add kvats tracker properties

---
+++ time/test/propl_kvats.py
@@ -0,0 +1,139 @@
+properties = \
+    [ ( 'area'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    , ( 'category'
+      , [ 'cert_sw'
+        , 'default_part_of'
+        , 'description'
+        , 'name'
+        , 'nosy'
+        , 'responsible'
+        , 'valid'
+        ]
+      )
+    , ( 'file'
+      , [ 'content'
+        , 'name'
+        , 'type'
+        ]
+      )
+    , ( 'issue'
+      , [ 'area'
+        , 'category'
+        , 'closed'
+        , 'composed_of'
+        , 'confidential'
+        , 'cur_est_begin'
+        , 'cur_est_end'
+        , 'deadline'
+        , 'depends'
+        , 'earliest_start'
+        , 'effective_prio'
+        , 'files'
+        , 'files_affected'
+        , 'fixed_in'
+        , 'keywords'
+        , 'kind'
+        , 'maturity_index'
+        , 'messages'
+        , 'needs'
+        , 'nosy'
+        , 'numeric_effort'
+        , 'part_of'
+        , 'planned_begin'
+        , 'planned_end'
+        , 'priority'
+        , 'release'
+        , 'responsible'
+        , 'severity'
+        , 'status'
+        , 'superseder'
+        , 'title'
+        ]
+      )
+    , ( 'keyword'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    , ( 'kind'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    , ( 'msg'
+      , [ 'author'
+        , 'content'
+        , 'date'
+        , 'files'
+        , 'header'
+        , 'inreplyto'
+        , 'keywords'
+        , 'messageid'
+        , 'recipients'
+        , 'subject'
+        , 'summary'
+        , 'type'
+        ]
+      )
+    , ( 'msg_keyword'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    , ( 'query'
+      , [ 'klass'
+        , 'name'
+        , 'private_for'
+        , 'tmplate'
+        , 'url'
+        ]
+      )
+    , ( 'severity'
+      , [ 'abbreviation'
+        , 'name'
+        , 'order'
+        ]
+      )
+    , ( 'status'
+      , [ 'description'
+        , 'name'
+        , 'order'
+        , 'transitions'
+        ]
+      )
+    , ( 'status_transition'
+      , [ 'name'
+        , 'require_msg'
+        , 'require_resp_change'
+        , 'target'
+        ]
+      )
+    , ( 'user'
+      , [ 'address'
+        , 'alternate_addresses'
+        , 'password'
+        , 'phone'
+        , 'queries'
+        , 'realname'
+        , 'roles'
+        , 'status'
+        , 'timezone'
+        , 'username'
+        ]
+      )
+    , ( 'user_status'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    ]
+
+if __name__ == '__main__' :
+    for cl, props in properties :
+        print cl
+        for p in props :
+            print '    ', p
