c7eb49126c83d63a027aabfd89e1819420dbe0af
Ralf Schlatterbeck <rsc@runtux.com>
2010-03-14 17:12:16 +0000

    Add it test

---
+++ time/test/propl_it.py
@@ -0,0 +1,107 @@
+properties = \
+    [ ( 'file'
+      , [ 'content'
+        , 'name'
+        , 'type'
+        ]
+      )
+    , ( 'it_category'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    , ( 'it_issue'
+      , [ 'category'
+        , 'confidential'
+        , 'deadline'
+        , 'files'
+        , 'it_prio'
+        , 'it_project'
+        , 'messages'
+        , 'nosy'
+        , 'responsible'
+        , 'stakeholder'
+        , 'status'
+        , 'superseder'
+        , 'title'
+        ]
+      )
+    , ( 'it_issue_status'
+      , [ 'description'
+        , 'name'
+        , 'order'
+        , 'relaxed'
+        , 'transitions'
+        ]
+      )
+    , ( 'it_prio'
+      , [ 'name'
+        , 'order'
+        ]
+      )
+    , ( 'it_project'
+      , [ 'category'
+        , 'confidential'
+        , 'deadline'
+        , 'files'
+        , 'it_prio'
+        , 'messages'
+        , 'nosy'
+        , 'responsible'
+        , 'stakeholder'
+        , 'status'
+        , 'title'
+        ]
+      )
+    , ( 'it_project_status'
+      , [ 'description'
+        , 'name'
+        , 'order'
+        , 'transitions'
+        ]
+      )
+    , ( 'msg'
+      , [ 'author'
+        , 'content'
+        , 'date'
+        , 'files'
+        , 'inreplyto'
+        , 'messageid'
+        , 'recipients'
+        , 'summary'
+        , 'type'
+        ]
+      )
+    , ( 'query'
+      , [ 'klass'
+        , 'name'
+        , 'private_for'
+        , 'tmplate'
+        , 'url'
+        ]
+      )
+    , ( 'user'
+      , [ 'address'
+        , 'alternate_addresses'
+        , 'password'
+        , 'phone'
+        , 'queries'
+        , 'realname'
+        , 'roles'
+        , 'status'
+        , 'timezone'
+        , 'username'
+        ]
+      )
+    , ( 'user_status'
+      , [ 'description'
+        , 'name'
+        ]
+      )
+    ]
+
+if __name__ == '__main__' :
+    for cl, props in properties :
+        print cl
+        for p in props :
+            print '    ', p
