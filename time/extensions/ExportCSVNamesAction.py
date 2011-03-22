a97f4027cefef7423037f3c617d63bfa7a125132
Ralf Schlatterbeck <rsc@runtux.com>
2011-03-22 17:10:41 +0000

    Back to timetuple with leading 0
    strftime doesn't seem to print leading 0

--- time/extensions/ExportCSVNamesAction.py
+++ time/extensions/ExportCSVNamesAction.py
@@ -635,7 +635,9 @@ class Export_CSV_Lielas (Export_CSV_Names) :
                     last_date = dt
                     line = [''] * (len (sids) + 2)
                     dt   = datetime (tzinfo = UTC, *dt.timetuple ()[:6])
-                    line [0] = dt.astimezone (TZ).strftime ('%d.%m.%Y %H:%M:%S')
+                    dt.astimezone (TZ).timetuple ()
+                    line [0] = '%02d.%02d.%04d %02d:%02d:%02d' \
+                        % (tp [2], tp [1], tp [0], tp [3], tp [4], tp [5])
                 line [index_by_sid [sens]] = "%2.2f" % val
         else :
             for itemid in self.klass.filter_iter \
