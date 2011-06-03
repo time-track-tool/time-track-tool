f4206065cea918f8daab40f7308d6c2e24ff9f32
Ralf Schlatterbeck <rsc@runtux.com>
2011-06-03 15:26:14 +0000

    Fix check for system users

--- time/extensions/ldap_rup_sync.py
+++ time/extensions/ldap_rup_sync.py
@@ -275,7 +275,8 @@ class LDAP_Roundup_Sync (object) :
             pass
         user  = uid and self.db.user.getnode (uid)
         # don't modify system users:
-        if user and user.status == self.status_system :
+        reserved = ('admin', 'anonymous')
+        if username in reserved or user and user.status == self.status_system :
             return
         luser = self.get_ldap_user_by_username (username)
         if not user and not luser :
