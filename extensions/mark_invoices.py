1e63286c9e82e45ea4f0ebcccf0a017c400da823
Ralf Schlatterbeck <rsc@runtux.com>
2014-03-12 15:08:50 +0100

    Remove ancient Id tags

--- extensions/mark_invoices.py
+++ extensions/mark_invoices.py
@@ -20,7 +20,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 # ****************************************************************************
-# $Id$
 
 from cStringIO                      import StringIO
 from os.path                        import splitext
