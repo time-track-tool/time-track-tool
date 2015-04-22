e914bfc788a9dab175181a3efa6a35b62290c833
Ralf Schlatterbeck <rsc@runtux.com>
2015-04-22 17:33:31 +0200

    Add assertion for valid dyn.vacation_yearly
    .. this helps better debug missing vacation_yearly for some users together
    with the url that was used to call this.

--- lib/vacation.py
+++ lib/vacation.py
@@ -502,6 +502,10 @@ def consolidated_vacation \
             continue
         assert not dyn.valid_to or dyn.valid_to > d
         eoy = roundup.date.Date ('%s-12-31' % d.year)
+        assert \
+            ( dyn.vacation_yearly is not None
+            , "vacation_yearly None for user_dynamic%s" % dyn.id
+            )
         if dyn.valid_to and dyn.valid_to <= ed and dyn.valid_to < eoy :
             yd = float (common.ydays (dyn.valid_to))
             vac += interval_days (dyn.valid_to - d) * dyn.vacation_yearly / yd
