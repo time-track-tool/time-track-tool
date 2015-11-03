f43eab29b2408fe6d01fbc233845920957a7fe54
Ralf Schlatterbeck <rsc@runtux.com>
2015-11-03 12:56:33 +0100

    Product sync script
    .. and some changes in locale see it_issue17191 msg724001.

--- utils/sync_products.py
+++ utils/sync_products.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-#from __future__ import unicode_literals
+from __future__ import unicode_literals
+from __future__ import print_function
 import os
 import re
 
@@ -12,40 +13,84 @@ splitre = re.compile (r'[ +_/&-]+')
 def normalize_name (name) :
     """ Normalize a name from different case etc.
         We assume unicode input.
-    >>> print normalize_name ('Projects + Other')
+    >>> print (normalize_name ('Projects + Other'))
     PROJECTS-OTHER
-    >>> print normalize_name ('  Projects + Other  ')
+    >>> print (normalize_name ('  Projects + Other  '))
     PROJECTS-OTHER
-    >>> print normalize_name ('PROJECTS-OTHER')
+    >>> print (normalize_name ('PROJECTS-OTHER'))
     PROJECTS-OTHER
-    >>> print normalize_name ('PROJECTS+OTHER')
+    >>> print (normalize_name ('PROJECTS+OTHER'))
     PROJECTS-OTHER
-    >>> print normalize_name ('PROJECTS-OTHERS')
+    >>> print (normalize_name ('PROJECTS-OTHERS'))
     PROJECTS-OTHER
-    >>> print normalize_name ('Other')
+    >>> print (normalize_name ('Other'))
     OTHER
-    >>> print normalize_name ('Others')
+    >>> print (normalize_name ('Others'))
     OTHER
-    >>> print normalize_name ('OTHERS')
+    >>> print (normalize_name ('OTHERS'))
     OTHER
-    >>> print normalize_name ('Customized Kits')
+    >>> print (normalize_name ('Customized Kits'))
     CUSTOMIZED-KITS
-    >>> print normalize_name ('CUSTOMIZED-KITS')
+    >>> print (normalize_name ('CUSTOMIZED-KITS'))
     CUSTOMIZED-KITS
-    >>> print normalize_name ('EV / Hybrid Vehicle ECU')
+    >>> print (normalize_name ('EV / Hybrid Vehicle ECU'))
     EV-HYBRID-VEHICLE-ECU
-    >>> print normalize_name (' EV / Hybrid Vehicle ECU ')
+    >>> print (normalize_name (' EV / Hybrid Vehicle ECU '))
     EV-HYBRID-VEHICLE-ECU
-    >>> print normalize_name (' APC&RD')
+    >>> print (normalize_name (' APC&RD'))
     APC-RD
+    >>> print (normalize_name (' 012345'))
+    12345
+    >>> print (normalize_name ('012345 '))
+    12345
     """
     x = '-'.join (l for l in splitre.split (name.upper ()) if l)
     if x == 'OTHERS' :
         x = 'OTHER'
     if x == 'PROJECTS-OTHERS' :
         x = 'PROJECTS-OTHER'
+    try :
+        x = str (int (x))
+    except ValueError :
+        pass
     return x
 # end def normalize_name
+
+class UTF8_Recoder (object) :
+
+    def __init__ (self, f) :
+        self.file = f
+    # end def __init__
+
+    def __iter__ (self) :
+        for line in self.file :
+            yield (line.encode ('utf-8'))
+    # end def __iter__
+
+# end class UTF8_Recoder
+
+
+class Unicode_DictReader (object) :
+
+    def __init__ (self, f, ** kw):
+        f = UTF8_Recoder (f)
+        self.reader = DictReader (f, ** kw)
+    # end def __init__
+
+    def __iter__ (self) :
+        ''' Returns the next dict line as a Unicode dict
+        '''
+        for d in self.reader :
+            r = []
+            for k, v in d.iteritems () :
+                k = k.decode ('utf-8')
+                if v is not None :
+                    v = v.decode ('utf-8')
+                r.append ((k, v))
+            yield dict (r)
+    # end def __iter__
+
+# end class Unicode_Reader
 
 class Product_Sync (object) :
 
@@ -69,14 +114,6 @@ class Product_Sync (object) :
             self.prodused [key] = False
             self.prodcats [key] = pd.id
 
-        self.bu_s     = {}
-        self.bu_used  = {}
-        for id in db.business_unit.getnodeids (retired = False) :
-            bu  = db.business_unit.getnode (id)
-            key = normalize_name (bu.name.decode ('utf-8'))
-            self.bu_used [key] = False
-            self.bu_s    [key] = bu.id
-
         self.products  = {}
         self.pr_used   = {}
         for id in db.product.getnodeids (retired = False) :
@@ -84,18 +121,134 @@ class Product_Sync (object) :
             key = normalize_name (pr.name.decode ('utf-8'))
             self.pr_used  [key] = False
             self.products [key] = pr.id
+        d_s = self.opt.delimiter.encode ('utf-8')
+        sap_dr = Unicode_DictReader \
+            (self.fixer_sap (), delimiter = d_s)
+        d_r = self.opt.radix_delimiter.encode ('utf-8')
+        rad_dr = Unicode_DictReader \
+            (self.fixer_rad (), delimiter = d_r)
+        self.debug ("SAP")
+        self.sap_recs = []
+        self.sap_ids  = {}
+        for x in sap_dr :
+            self.debug (repr (x))
+            id = self.get_material (x)
+            if id in self.sap_ids :
+                self.verbose ('Ignoring duplicate: %r' % x)
+                continue
+            self.sap_ids [id] = len (self.sap_recs)
+            self.sap_recs.append (x)
+        self.debug ("RADIX")
+        self.rad_recs = []
+        self.rad_ids  = {}
+        for x in rad_dr :
+            self.debug (repr (x))
+            id = self.get_material (x)
+            if id in self.rad_ids :
+                self.verbose ('Ignoring duplicate: %r' % x)
+                continue
+            self.rad_ids [id] = len (self.rad_recs)
+            self.rad_recs.append (x)
+        for id in self.rad_ids :
+            try :
+                id = int (id)
+            except ValueError :
+                self.warn ("Ignoring Radix ID: %s" % id)
+            if id > 11500 :
+                self.warn ("Ignoring Radix ID: %s" % id)
+            # Radix wins for IDs < 11500
+            if str (id) in self.sap_ids :
+                self.warn ("Ignoring SAP ID: %s (also in Radix)" % id)
+                del self.sap_recs [self.sap_ids [str (id)]]
+                del self.sap_ids  [str (id)]
+        for id in self.sap_ids :
+            assert id not in self.rad_ids
     # end def __init__
 
-    def sync (self) :
-        dr = DictReader \
-            (open (self.args [0], 'r'), delimiter = self.opt.delimiter)
+    def debug (self, text) :
+        if self.opt.debug :
+            print (text)
+    # end def debug
 
+    def get_material (self, rec) :
+        for n in 'Material', 'Artikelkode' :
+            try :
+                v = rec [n]
+                break
+            except KeyError :
+                pass
+        else :
+            raise
+        try :
+            v = str (int (v))
+        except ValueError :
+            pass
+        return v
+    # end def get_material
+
+    def get_description (self, rec) :
+        for n in 'Materialkurztext', 'Bezeichnung' :
+            try :
+                v = rec [n]
+                break
+            except KeyError :
+                pass
+        else :
+            raise
+        return v
+    # end def get_description
+
+    def get_oldcode (self, rec) :
+        for n in 'Alte Materialnr.', 'Alter Artikelkode' :
+            try :
+                v = rec [n]
+                break
+            except KeyError :
+                pass
+        else :
+            raise
+        try :
+            v = str (int (v))
+        except ValueError :
+            pass
+        return v
+    # end def get_oldcode
+
+    def fixer_sap (self) :
+        with codecs.open (self.args [0], 'r', self.opt.encoding) as f :
+            l = f.next ()
+            assert 'MM Report Material Stammdaten' in l
+            l = f.next ()
+            assert l.strip () == ''
+            l = f.next ()
+            assert l.startswith ('MM Report Material Stammdaten')
+            l = f.next ()
+            assert l.strip () == ''
+            l = f.next ()
+            assert l.startswith ('%sMaterial' % self.opt.delimiter)
+            yield (('Ignore-me%s' % l))
+
+            for line in f :
+                yield (line)
+    # end def fixer_sap
+
+    def fixer_rad (self) :
+        with codecs.open (self.args [1], 'r', self.opt.radix_encoding) as f :
+            for line in f :
+                yield (line)
+    # end def fixer_rad
+
+    def sync (self) :
         skey = lambda x : x [1]
-        for rec in dr :
+        for rec in self.rad_recs + self.sap_recs :
+            if not rec ['Product Family'] :
+                self.warn ('Ignoring: %r' % rec)
+                continue
             pcats = []
             for k, lvl in sorted (self.levels.iteritems (), key = skey) :
-                v = rec [k].strip ().decode (self.opt.encoding)
+                v = rec [k].strip ()
                 if not v or v == '0' or v == '1' :
+                    self.warn ('Invalid record: %s=%s' % (k, v))
                     break
                 key = (normalize_name (v), lvl)
                 par = dict \
@@ -107,64 +260,76 @@ class Product_Sync (object) :
                     (self.db.prodcat, self.prodcats, self.prodused, key, par)
                 pcats.append (r)
 
-            v   = rec ['BU Owner'].decode (self.opt.encoding)
-            try :
-                sbu = rec ['Selling BU'].decode (self.opt.encoding)
-            except KeyError :
-                sbu = rec ['BU Owner'].decode (self.opt.encoding)
-            if sbu == u'0' or sbu == u'ALL-BUSINESS-UNITS' :
-                sbu = []
-            else :
-                sbu = sbu.split ('+')
-                sbu = [normalize_name (x) for x in sbu]
-            key = normalize_name (v)
-            par = dict (name = v.encode ('utf-8'), valid = True)
-            bu  = None
-            if v and v != 'ALL-BUSINESS-UNITS' and v != '0' :
-                bu  = self.update_table \
-                    (self.db.business_unit, self.bu_s, self.bu_used, key, par)
+            v = self.get_material    (rec)
+            d = self.get_description (rec)
+            a = self.get_oldcode     (rec)
+            if a == '-' :
+                a = None
+            n = a
+            if not n :
+                n = v
 
-            try :
-                v   = rec ['Artikelnummer'].decode (self.opt.encoding)
-            except KeyError :
-                v   = rec ['Article Code'].decode (self.opt.encoding)
-            try :
-                d   = rec ['Artikelbeschreibung'].decode (self.opt.encoding)
-            except KeyError :
-                d   = rec ['Description'].decode (self.opt.encoding)
-            if bu and sbu and key not in sbu :
-                if self.opt.verbose :
-                    print "Skipping %s Not owning BU: %s (%s)" % (v, sbu, key)
-                continue
-            key = normalize_name (v)
+            key  = normalize_name (n)
+            key2 = normalize_name (v)
+            if key and key2 and key != key2 :
+                k1 = k1 = None
+                try :
+                    k1 = int (key)
+                    k2 = int (key2)
+                except ValueError :
+                    pass
+                if k1 and k2 and k1 != k2 :
+                    self.warn \
+                        ( "Differing numeric Old/New material number: %s %s"
+                        % (k1, k2)
+                        )
+                    key2 = a = None
+                    n = v
+               
             if v and v != '0' and len (pcats) == 3 :
                 par = dict \
-                    ( name             = v.encode ('utf-8')
+                    ( name             = n.encode ('utf-8')
                     , description      = d.encode ('utf-8')
-                    , business_unit    = bu
-                    , is_series        = False
+                    , sap_material     = v.encode ('utf-8')
                     , valid            = True
                     , product_family   = pcats [2]
                     , product_use_case = pcats [1]
                     , product_line     = pcats [0]
                     )
+                self.debug (repr (par))
                 p = self.update_table \
-                    (self.db.product, self.products, self.pr_used, key, par)
+                    ( self.db.product
+                    , self.products
+                    , self.pr_used
+                    , key
+                    , par
+                    , key2
+                    )
         self.validity (self.db.prodcat,       self.prodcats, self.prodused)
-        self.validity (self.db.business_unit, self.bu_s,     self.bu_used)
         self.validity (self.db.product,       self.products, self.pr_used)
         if self.opt.update :
             self.db.commit()
     # end def sync
 
-    def update_table (self, cls, nodedict, usedict, key, params) :
-        if key in nodedict :
+    def update_table (self, cls, nodedict, usedict, key, params, key2 = None) :
+        assert key or key2
+        if key2 and key != key2 and key in nodedict and key2 in nodedict :
+            self.warn \
+                ("Product %s with material-num %s found twice: %s/%s"
+                % (key, key2, nodedict [key], nodedict [key2])
+                )
+            return None
+        k = key
+        if key2 in nodedict :
+            k = key2
+        if k in nodedict :
             # Update name on first match if we have a new spelling
-            if not usedict [key] :
-                node = cls.getnode (nodedict [key])
+            if not usedict [k] :
+                node = cls.getnode (nodedict [k])
                 d = {}
-                if node.name != params ['name'] :
-                    d ['name'] = params ['name']
+                # We don't update name: This could only mean that up to
+                # now we had the SAP-Name (which is fine) and found some
+                # long-obsolete old-name in Radix.
                 if 'parent' in params and node.parent != params ['parent'] :
                     d ['parent'] = params ['parent']
                 if 'prodcat' in params and node.prodcat != params ['prodcat'] :
@@ -172,37 +337,50 @@ class Product_Sync (object) :
                 for a in 'product_family', 'product_line', 'product_use_case' :
                     if a in params and getattr (node, a) != params [a] :
                         d [a] = params [a]
-                bu = 'business_unit'
-                if bu in params and node.business_unit != params [bu] :
-                    d [bu] = params [bu]
+                sm = params.get ('sap_material')
+                if sm and node.sap_material != sm :
+                    d ['sap_material'] = sm
                 if d :
-                    cls.set (nodedict [key], ** d)
-                    if self.opt.verbose :
-                        print "Update %s: %s: %s" % (cls.classname, key, d)
+                    self.verbose ("Update %s: %s: %s" % (cls.classname, k, d))
+                    if self.opt.update :
+                        cls.set (nodedict [k], ** d)
         else :
-            id = cls.create (** params)
-            if self.opt.verbose :
-                print "Create %s: %s: %s" % (cls.classname, key, params)
-            nodedict [key] = id
-        usedict [key] = True
-        return nodedict [key]
+            if self.opt.update :
+                id = cls.create (** params)
+            else :
+                id = '999999' # fake id
+            self.verbose \
+                ("Create %s%s: %s: %s" % (cls.classname, id, k, params))
+            nodedict [k] = id
+        usedict [k] = True
+        return nodedict [k]
     # end def update_table
 
     def validity (self, cls, nodedict, usedict) :
         for k, v in usedict.iteritems () :
             id = nodedict [k]
-            if v or not self.opt.noinvalidate :
-                if not v and self.opt.verbose :
-                    print "Invalidating %s: %s" % (cls.classname, k)
-                cls.set (id, valid = v)
+            if v or self.opt.invalidate :
+                if not v :
+                    self.verbose ("Invalidating %s: %s" % (cls.classname, k))
+                if self.opt.update :
+                    cls.set (id, valid = v)
     # end def validity
+
+    def verbose (self, text) :
+        if self.opt.verbose :
+            print (text)
+    # end def verbose
+
+    def warn (self, text) :
+        print ('Warning: %s' % text)
+    # end def warn
 
 # end class Product_Sync
 
 def main () :
     dir     = os.getcwd ()
 
-    cmd = OptionParser ("Usage: %prog [options] inputfile")
+    cmd = OptionParser ("Usage: %prog [options] sap-inputfile radix-inputfile")
     cmd.add_option \
         ( '-d', '--directory'
         , dest    = 'dir'
@@ -210,21 +388,39 @@ def main () :
         , default = dir
         )
     cmd.add_option \
-        ( '-D', '--Delimiter'
+        ( '--debug'
+        , dest    = 'debug'
+        , help    = 'Debug output'
+        , action  = 'store_true'
+        )
+    cmd.add_option \
+        ( '-D', '--sap-delimiter'
         , dest    = 'delimiter'
-        , help    = 'CSV delimiter'
+        , help    = 'CSV delimiter for SAP input-file'
         , default = '\t'
         )
     cmd.add_option \
-        ( '-E', '--Encoding'
-        , dest    = 'encoding'
-        , help    = 'CSV character encoding'
-        , default = 'utf-8'
+        ( '-R', '--radix-delimiter'
+        , dest    = 'radix_delimiter'
+        , help    = 'CSV delimiter for Radix input-file'
+        , default = '\t'
         )
     cmd.add_option \
-        ( '-i', '--do-not-invalidate'
-        , dest   = 'noinvalidate'
-        , help   = 'Do not invalidate if not in import file'
+        ( '-E', '--encoding'
+        , dest    = 'encoding'
+        , help    = 'CSV character encoding for SAP input-file'
+        , default = 'latin1'
+        )
+    cmd.add_option \
+        ( '-e', '--radix-encoding'
+        , dest    = 'radix_encoding'
+        , help    = 'CSV character encoding for Radix input-file'
+        , default = 'utf-16'
+        )
+    cmd.add_option \
+        ( '-i', '--invalidate'
+        , dest   = 'invalidate'
+        , help   = 'Invalidate if not in import file'
         , action = 'store_true'
         )
     cmd.add_option \
@@ -240,8 +436,8 @@ def main () :
         , action = 'store_true'
         )
     opt, args = cmd.parse_args ()
-    if len (args) != 1 :
-        cmd.error ('Need input file')
+    if len (args) != 2 :
+        cmd.error ('Need two input files')
         sys.exit  (23)
 
     ps = Product_Sync (opt, args)
