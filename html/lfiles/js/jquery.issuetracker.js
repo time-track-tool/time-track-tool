bfc3ebfdf19a5ee1925081b0983fe64a384d497a
Ralf Schlatterbeck <rsc@runtux.com>
2016-04-06 13:42:38 +0200

    Fix text wrapping
    Now new messages no longer use a textarea with wrap=hard. The display of
    messages is changed to no longer use <pre> but a div with a monospace
    font and white-space: pre-wrap.

--- html/lfiles/js/jquery.issuetracker.js
+++ html/lfiles/js/jquery.issuetracker.js
@@ -2,29 +2,29 @@
  * jQuery JavaScript Extention - Issue Tracker
  * http://jquery.com/
  *
- * Author: 	NSC
- * Date:	2015-09-01
- * Verison:	0.2s 
+ * Author:      NSC, modified RSC for new layout (without <pre> tag)
+ *              also move css to stylesheet
+ * Date:        2015-09-01
  */
 $(document).ready(function() {
     $("table.messages td.content").each(function() {
-     	 if($(this).html().indexOf("-----Original Message-----") > -1) {
-            $(this).find("pre").wrapInner("<div class='pre'></div>"); 
-            $(this).find("div").unwrap(); 
+         if($(this).html().indexOf("-----Original Message-----") > -1) {
+            $(this).find("div").wrapInner("<div class='pre'></div>");
+            $(this).find("div.pre").unwrap();
             var oContent = $(this).find(".pre").html();
-            var pos = oContent.indexOf("-----Original Message-----");             
-            var nContent = oContent.substr(0,(pos-1));    
-            var nAttachment = oContent.substr(pos,oContent.length);   
-            $(this).find(".pre").html(nContent).wrapInner("<pre></pre>");
+            var pos = oContent.indexOf("-----Original Message-----");
+            var nContent = oContent.substr(0,(pos-1));
+            var nAttachment = oContent.substr(pos,oContent.length);
+            $(this).find(".pre").html(nContent).wrapInner("<div class='prewrap'></div>");
             var trigger = $("<a href='javascript:void(0)'><span>▶</span> attached mail</a>").appendTo($(this).find(".pre"));
-            $(trigger).css({'text-decoration':'none','border':'1px solid #CCC','border-radius':'2px','padding':'3px'});             
-            var mail = $("<div>"+nAttachment+"</div>").appendTo($(this).find("div"));           
-            $(mail).addClass('attachment').hide().css({'margin-left':'20px','border-left':'1px solid #CCC','padding-left':'5px'});             
-            $(mail).wrapInner("<pre></pre>");
+            $(trigger).addClass('toggle')
+            var mail = $("<div>"+nAttachment+"</div>").appendTo($(this).find("div.pre"));
+            $(mail).addClass('attachment').hide();
             $(trigger).click(function() {
-            	$(this).parent("div").find(".attachment").slideToggle();
-                    if($(this).find("span").html() == "▼") {$(this).find("span").html("▶");} else {$(this).find("span").html("▼");}  
-            });              
+                $(this).parent("div").find(".attachment").slideToggle();
+                    if($(this).find("span").html() == "▼") {$(this).find("span").html("▶");} else {$(this).find("span").html("▼");}
+            });
+            $(this).find("div.pre").addClass('prewrap').delClass('pre')
          }
     });
 });
