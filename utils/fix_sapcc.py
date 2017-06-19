8c53e86f59ab7efcef12d87233abc6e5fbc96034
Ralf Schlatterbeck <rsc@runtux.com>
2017-06-19 12:15:12 +0200

    Update sap_cc fixer for name/desc split

--- utils/fix_sapcc.py
+++ utils/fix_sapcc.py
@@ -9,6 +9,9 @@ db      = tracker.open ('admin')
 
 for id in db.sap_cc.getnodeids (retired = False) :
     sc = db.sap_cc.getnode (id)
+    if len (sc.name) != 7 and not sc.description :
+        name, desc = sc.name.split ('_', 1)
+        db.sap_cc.set (id, name = name, description = desc)
     if sc.valid is not None :
         continue
     if 'gelöscht' in sc.name :
