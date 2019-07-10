6691e39fe49ffa41083266ded20db3ea48197d5a
Robert Klonner <robert.klonner@opensource.tttech.com>
2019-07-10 16:13:57 +0200

    Instanciate select2 on work package select element

---
+++ html/lfiles/js/daily_record_advanced.js
@@ -0,0 +1,6 @@
+/* javascript code to extend Time Tracker daily record function */
+$(document).ready(function() {
+    /* instanciate jquery select2 plungin on select html elements of
+       of class dropdown-wp to make them searchable */
+    $(".dropdown-wp").select2();
+});
