735a61afe958de44314f09d9991b68e022d52934
Ralf Schlatterbeck <rsc@runtux.com>
2021-04-29 13:13:32 +0200

    Add update of existing docs

--- utils/doc-cat-lifter.py
+++ utils/doc-cat-lifter.py
@@ -8,18 +8,34 @@ dir     = os.getcwd ()
 tracker = instance.open (dir)
 db      = tracker.open ('admin')
 
+cat_by_doc_num = {}
 modified = False
 with open ('doc_cat.csv', 'r') as f :
     dr = csv.DictReader (f, delimiter = ';')
     for rec in dr :
-        name = rec ['name']
+        name    = rec ['name']
+        doc_num = rec ['doc_num']
         try :
-            db.doc_category.lookup (name)
+            id = db.doc_category.lookup (name)
         except KeyError :
-            db.doc_category.create \
+            id = db.doc_category.create \
                 ( name    = name
-                , doc_num = rec ['doc_num']
+                , doc_num = doc_num
                 )
             modified = True
+        cat_by_doc_num [doc_num] = id
+# Now loop over all docs and look up the correct doc_category via the
+# department and set the doc_category
+
+for did in db.doc.getnodeids (retired = False) :
+    doc = db.doc.getnode (did)
+    if doc.doc_category :
+        continue
+    dep = db.department.getnode (doc.department)
+    assert dep.doc_num
+    doc_cat = cat_by_doc_num [dep.doc_num]
+    db.doc.set (did, doc_category = doc_cat)
+    modified = True
+
 if modified :
     db.commit ()
