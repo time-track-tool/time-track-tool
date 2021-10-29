efafb2904c723a0ab9d1ed1bac5da26197343eca
Ralf Schlatterbeck <rsc@runtux.com>
2021-10-29 11:40:31 +0200

    Do not create on-call duty permission
    This fixes the rollout script utils/time_activity_lift.py
    We now *can* add permissions to time_activity but for now none are
    created.

--- utils/time_activity_lift.py
+++ utils/time_activity_lift.py
@@ -7,19 +7,19 @@ dir     = os.getcwd ()
 tracker = instance.open (dir)
 db      = tracker.open ('admin')
 
-try :
-    tap = db.time_activity_perm.lookup ('on-call duty')
-except KeyError :
-    tap = db.time_activity_perm.create \
-        ( name        = 'on-call duty'
-        , description = 'Allowed to book on on-call duty'
-        )
-
-act = db.time_activity.getnode ('24')
-assert 'on-call' in act.name
-
-if not act.time_activity_perm :
-    db.time_activity.set (act.id, time_activity_perm = tap)
+# Do *not* create the on-call duty permission:
+#try :
+#    tap = db.time_activity_perm.lookup ('on-call duty')
+#except KeyError :
+#    tap = db.time_activity_perm.create \
+#        ( name        = 'on-call duty'
+#        , description = 'Allowed to book on on-call duty'
+#        )
+#act = db.time_activity.getnode ('24')
+#assert 'on-call' in act.name
+#
+#if not act.time_activity_perm :
+#    db.time_activity.set (act.id, time_activity_perm = tap)
 
 for id in '10', '11', '24' :
     if db.time_activity.get (id, 'is_valid') is None :
