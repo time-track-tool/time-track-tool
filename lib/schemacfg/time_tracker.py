04434bba610473f57c0ebe74180e1499bc67c0b3
Ralf Schlatterbeck <rsc@runtux.com>
2021-12-09 17:26:38 +0100

    Allow user to search vacation_correction

--- lib/schemacfg/time_tracker.py
+++ lib/schemacfg/time_tracker.py
@@ -1671,6 +1671,12 @@ def security (db, ** kw) :
     db.security.addPermissionToRole ('User', p)
 
     p = db.security.addPermission \
+        ( name        = 'Search'
+        , klass       = 'vacation_correction'
+        )
+    db.security.addPermissionToRole ('User', p)
+
+    p = db.security.addPermission \
         ( name        = 'View'
         , klass       = 'vacation_correction'
         , check       = own_vacation_correction
