5ec1f45220ca0935cb3f8c631bcbb03c281e9878
Ralf Schlatterbeck <rsc@runtux.com>
2023-09-01 16:48:29 +0200

    Do not print abs vacation correction on Jan 1st
    These do not change anything in the computation (except time taken over
    from pervious year).

--- extensions/summary.py
+++ extensions/summary.py
@@ -1945,6 +1945,11 @@ class Vacation_Report (_Report):
                             item  = self.htmldb.vacation_correction.getItem (x)
                             days  = item.days
                             ep    = self.utils.ExtProperty
+                            # Do not print abs vac corrections on Jan first
+                            # These do not change the display
+                            dt    = item.date
+                            if item.absolute and d.month == 1 and d.day == 1:
+                                continue
                             vcs.append \
                                 ( ep
                                     ( self.utils, days 
@@ -1954,9 +1959,16 @@ class Vacation_Report (_Report):
                                 )
                         container ['vacation corrections'] = vcs
                     except AttributeError:
+                        gn = db.vacation_correction.getnode
+                        vcss = [gn (i) for i in vcids]
+                        # Do not print abs vac corrections on Jan first
+                        # These do not change the display
                         container ['vacation corrections'] = ' + '.join \
-                            (str (db.vacation_correction.get (i, 'days'))
-                             for i in vcids
+                            ( str (v.days) for v in vcss
+                              if (  not v.absolute
+                                 or v.date.month != 1
+                                 or v.date.day != 1
+                                 )
                             )
                     if (u, ctype) not in self.values:
                         self.values [(u, ctype)] = []
