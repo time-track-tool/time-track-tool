time-track-tool/time-track-tool

commit 51bc0232fd642b3fc8bb70f93524e62b9718a042

Special leave changes in tests

detectors/d_vacation.py
@@ -546,7 +546,7 @@ def handle_accept (db, vs, ars, trs, old_status):
                 ( db, vs, now
                 , 'MAIL_SPECIAL_LEAVE_NOTIFY_TEXT'
                 , 'MAIL_SPECIAL_LEAVE_NOTIFY_SUBJECT'
-                , email = get_leave_email (db, vs)
+                , 'MAIL_SPECIAL_LEAVE_NOTIFY_EMAIL'
                 )
 # end def handle_accept
 

test/test_base.py
@@ -3331,7 +3331,7 @@ class Test_Case_Timetracker (_Test_Case_Summary, unittest.TestCase) :
             [ ( ('subject',    'Leave request "Special Leave/Special" '
                                '2010-12-22 to 2010-12-30 from Test User2')
               , ('precedence', 'bulk')
-              , ('to',         'user1@test.test')
+              , ('to',         'user1@test.test, user0@test.test')
               , ('from',       'roundup-admin@'
                                'your.tracker.email.domain.example')
               )
@@ -3347,7 +3347,7 @@ class Test_Case_Timetracker (_Test_Case_Summary, unittest.TestCase) :
             [ b'Test User2 has submitted a leave request\n'
               b'"Special Leave/Special".\n'
               b'Comment from user: Special leave comment\n'
-              b'Please approve or decline at\n'
+              b'Needs approval by HR.\n'
               b'http://localhost:4711/ttt/leave_submission?@template=approve'
               b'\nMany thanks!'
               b'\n\nThis is an automatically generated message.\n'

commit 08ad04bcee940d9c859c795924c9b5e1ba51cc18

Fix sending of email about special leaves
This apparently got broken in commit
51bc0232fd642b3fc8bb70f93524e62b9718a042
and is probably due to rebasing at some point (not the latest rebase,
though)

detectors/d_vacation.py
@@ -546,7 +546,7 @@ def handle_accept (db, vs, ars, trs, old_status):
                 ( db, vs, now
                 , 'MAIL_SPECIAL_LEAVE_NOTIFY_TEXT'
                 , 'MAIL_SPECIAL_LEAVE_NOTIFY_SUBJECT'
-                , 'MAIL_SPECIAL_LEAVE_NOTIFY_EMAIL'
+                , email = get_leave_email (db, vs)
                 )
 # end def handle_accept
 