629295f91b04ce4152cb48dc175cc2f186dadab0
Ralf Schlatterbeck <rsc@runtux.com>
2025-09-04 11:08:27 +0200

    Add uid handling
    Create uid if missing, do not allow changes (unless the uid field is
    empty).
    Fix coding style.

--- detectors/address.py
+++ detectors/address.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2004-10 Ralf Schlatterbeck. All rights reserved
+# Copyright (C) 2004-25 Ralf Schlatterbeck. All rights reserved
 # Reichergasse 131, A-3411 Weidling
 # ****************************************************************************
 #
@@ -17,6 +17,7 @@
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 # ****************************************************************************
 
+from uuid                           import uuid4
 from roundup.exceptions             import Reject
 from roundup.date                   import Date
 
@@ -24,22 +25,22 @@ from rup_utils                      import translate
 
 import common
 
-def fix_adr_type (db, cl, nodeid, new_values) :
-    if 'adr_type' in new_values :
+def fix_adr_type (db, cl, nodeid, new_values):
+    if 'adr_type' in new_values:
         type_cat  = db.adr_type_cat.lookup ('ABO')
         adr_types = db.adr_type.find (typecat = type_cat)
         abos = new_values.get ('abos', None)
-        if abos is None and nodeid :
+        if abos is None and nodeid:
             abos = cl.get (nodeid, 'abos')
-        else :
+        else:
             abos = []
         abos = [db.abo.getnode (a) for a in abos]
         adr_type_dict = dict ([(a, 1) for a in new_values ['adr_type']])
-        for t in adr_types :
-            if t in adr_type_dict :
+        for t in adr_types:
+            if t in adr_type_dict:
                 del adr_type_dict [t]
-        for abo in abos :
-            if not abo ['end'] or abo ['end'] > Date ('.') :
+        for abo in abos:
+            if not abo ['end'] or abo ['end'] > Date ('.'):
                 abotype = db.abo_price.get (abo ['aboprice'], 'abotype')
                 adrtype = db.abo_type.get  (abotype, 'adr_type')
                 assert (adrtype)
@@ -47,86 +48,103 @@ def fix_adr_type (db, cl, nodeid, new_values) :
         new_values ['adr_type'] = list (adr_type_dict)
 # end def fix_adr_type
 
-def check_retire_address (db, cl, nodeid, old_values) :
+def check_retire_address (db, cl, nodeid, old_values):
         oadr = None
-        if old_values :
+        if old_values:
             oadr = old_values.get ('address')
             nadr = db.person.get (nodeid, 'address')
-            if nadr and db.address.is_retired (nadr) :
+            if nadr and db.address.is_retired (nadr):
                 db.address.restore (nadr)
-        else :
+        else:
             oadr = cl.get (nodeid, 'address')
-        if oadr and not common.persons_for_adr (db, oadr) :
+        if oadr and not common.persons_for_adr (db, oadr):
             db.address.retire (oadr)
 # end def check_retire_address
 
-def set_adr_defaults (db, cl, nodeid, new_values) :
+def set_adr_defaults (db, cl, nodeid, new_values):
     """ Set some default values for new address """
-    if 'lettertitle' in cl.properties :
-        if 'lettertitle' not in new_values  and 'title' in new_values :
+    if 'lettertitle' in cl.properties:
+        if 'lettertitle' not in new_values  and 'title' in new_values:
             new_values ['lettertitle'] = new_values ['title']
-    if 'valid' in cl.properties and 'valid' not in new_values :
+    if 'valid' in cl.properties and 'valid' not in new_values:
             new_values ['valid'] = '1'
-    if 'firstname' in cl.properties :
+    if 'firstname' in cl.properties:
         if (   'initial' not in new_values
            and 'firstname'   in new_values and new_values ['firstname']
-           ) :
+           ):
             new_values ['initial'] = new_values ['firstname'][0].upper () + '.'
 # end def set_adr_defaults
 
-def require_country (db, cl, nodeid, new_values) :
-    if not cl.key :
+def require_country (db, cl, nodeid, new_values):
+    if not cl.key:
         common.require_attributes \
             (db.i18n.gettext, cl, nodeid, new_values, 'country')
 # end def require_country
 
-def check_function (db, cl, nodeid, new_values) :
+def check_function (db, cl, nodeid, new_values):
     return common.check_attribute_lines \
         (db.i18n.gettext, new_values, 'function', 2)
 # end def check_function
 
-def require_cust_supp (db, cl, nodeid, new_values) :
-    if 'cust_supp' in cl.properties :
+def require_cust_supp (db, cl, nodeid, new_values):
+    if 'cust_supp' in cl.properties:
         common.require_attributes \
             (db.i18n.gettext, cl, nodeid, new_values, 'cust_supp')
 # end def require_cust_supp
 
-def check_retire (db, cl, nodeid, old_values) :
-    if 'cust_supp' in cl.properties and cl.get (nodeid, 'cust_supp') is None :
+def check_retire (db, cl, nodeid, old_values):
+    if 'cust_supp' in cl.properties and cl.get (nodeid, 'cust_supp') is None:
         cl.retire (nodeid)
 # end def check_retire
 
-def fix_is_valid (db, cl, nodeid, new_values) :
-    if 'is_valid' not in new_values :
+def fix_is_valid (db, cl, nodeid, new_values):
+    if 'is_valid' not in new_values:
         new_values ['is_valid'] = True
 # end def fix_is_valid
 
-def init (db) :
+def set_uid (db, cl, nodeid, new_values):
+    if 'uid' not in new_values:
+        new_values ['uid'] = str (uuid4 ())
+# end def set_uid
+
+def require_uid (db, cl, nodeid, new_values):
+    if 'uid' in new_values:
+        # Allow set only if it was empty previously
+        if cl.get (nodeid, 'uid'):
+            raise Reject ('The uid value must not change')
+# end def require_uid
+
+def init (db):
     adrclass = persclass = None
-    if 'address' in db.classes :
+    if 'address' in db.classes:
         adrclass  = persclass = db.address
-    if 'person' in db.classes :
+    if 'person' in db.classes:
         persclass = db.person
-    if 'customer' in db.classes :
+    if 'customer' in db.classes:
         adrclass  = persclass = db.customer
         adrclass.audit   ("create", fix_is_valid)
-
-    if 'abo' in db.classes and adrclass :
+    if 'abo' in db.classes and adrclass:
         adrclass.audit   ("create", fix_adr_type)
         adrclass.audit   ("set",    fix_adr_type)
-    if adrclass :
+    if adrclass:
         adrclass.audit   ("create", common.lookalike_computation)
         adrclass.audit   ("set",    common.lookalike_computation)
         adrclass.audit   ("create", require_country)
         adrclass.audit   ("set",    require_country)
         adrclass.audit   ("create", set_adr_defaults)
-    if persclass and persclass != adrclass :
+        if 'uid' in adrclass.properties:
+            adrclass.audit ("create", set_uid)
+            adrclass.audit ("set",    require_uid)
+    if persclass and persclass != adrclass:
         persclass.audit  ("create", common.lookalike_computation)
         persclass.audit  ("set",    common.lookalike_computation)
         persclass.audit  ("create", set_adr_defaults)
         persclass.react  ("set",    check_retire_address)
         persclass.react  ("retire", check_retire_address)
-    if persclass :
+        if 'uid' in persclass.properties:
+            persclass.audit ("create", set_uid)
+            persclass.audit ("set",    require_uid)
+    if persclass:
         persclass.audit  ("create", require_cust_supp)
         persclass.audit  ("create", check_function)
         persclass.audit  ("set",    check_function)
