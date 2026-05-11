f9cde0ab62146e82c372829dd20f78f61248c1f4
Ralf Schlatterbeck <rsc@runtux.com>
2026-05-11 10:04:38 +0200

    Unfreeze users

--- utils/tt_lift.py
+++ utils/tt_lift.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 import os
-from roundup           import instance
+from roundup import instance, date
 dir     = os.getcwd ()
 tracker = instance.open (dir)
 db      = tracker.open ('admin')
@@ -27,5 +27,25 @@ for id in db.vac_aliq.getnodeids (retired = False):
 # Create remaining countries
 for c in sorted (countries):
     db.vac_aliq.create (name = c)
+
+# Permission for a user
+
+# Unfreeze existing users until jan
+# First find dyn users for org_location
+
+dynids = db.user_dynamic.filter (None, dict (org_location = '34'))
+users = set ()
+for id in dynids:
+    dyn = db.user_dynamic.getnode (id)
+    users.add (dyn.user)
+srt = [('+', 'user'), ('-', 'date')]
+freeze = db.daily_record_freeze.filter \
+    (None, dict (user = list (users)), sort = srt)
+
+cutoff = date.Date ('2026-01-01')
+for f in freeze:
+    frz = db.daily_record_freeze.getnode (f)
+    if frz.date > cutoff and frz.frozen:
+        db.daily_record_freeze.set (f, frozen = False)
 
 db.commit()
